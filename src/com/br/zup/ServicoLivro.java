33bbf433f5f84143d326220a89a06d2bd4e8a368
Rodrigo Vaz De Oliveira <rodrigovazdeoliveira@gmail.com>
2021-02-26 17:02:31 -0300

    Refatorando métodos do servicoLivro

--- src/com/br/zup/ServicoLivro.java
+++ src/com/br/zup/ServicoLivro.java
@@ -11,12 +11,11 @@ import java.util.List;
  * - listar os livros
  * */
 public class ServicoLivro {
-
     private static List<Livro>livros = new ArrayList<>();
 
     public static Livro cadastrarNovoLivro(String autor, String titulo, String editora, int exemplares, Categoria categoria){
         Livro novoLivro;
-        novoLivro = new Livro( autor, titulo, editora, categoria, exemplares );
+        novoLivro = new Livro( autor, titulo, editora, categoria, exemplares);
         livros.add(novoLivro);
         return novoLivro;
     }
@@ -28,51 +27,59 @@ public class ServicoLivro {
      * */
     public static String ListarTodosOsLivros() {
         String livroInformacao = "";
+
         for(Livro livro : livros) {
             livroInformacao += livro.toString();
         }
+
         return livroInformacao;
     }
 
     public static List<Livro> buscarLivroPorAutor(String autor) throws Exception {
         List<Livro>livrosPesquisados = new ArrayList<>();
+
         for (Livro livro: livros) {
             if (livro.getAutor().equalsIgnoreCase(autor)){
                 livrosPesquisados.add(livro);
             }
         }
+
         if (!livrosPesquisados.isEmpty()) {
             return livrosPesquisados;
-        } else {
-            throw new Exception("Nenhum livro foi localizado pelo autor " + autor);
         }
+
+        throw new Exception("Nenhum livro foi localizado pelo autor " + autor);
     }
 
     public static List<Livro> buscarLivroPorEditora(String editora) throws Exception {
         List<Livro>livrosPesquisados = new ArrayList<>();
+
         for (Livro livro: livros) {
             if (livro.getEditora().equalsIgnoreCase(editora)){
                 livrosPesquisados.add(livro);
             }
         }
+
         if (!livrosPesquisados.isEmpty()) {
             return livrosPesquisados;
-        } else {
-            throw new Exception("Nenhum livro foi localizado com a editora  " + editora);
         }
+
+        throw new Exception("Nenhum livro foi localizado com a editora  " + editora);
     }
 
     public static List<Livro> pesquisarLivroPorCategoria(Categoria categoria) throws Exception {
         List<Livro> livrosPorCategoria = new ArrayList<>();
+
         for (Livro livro : livros) {
             if (livro.getCategoria() == categoria) {
                 livrosPorCategoria.add(livro);
             }
         }
+
         if (!livrosPorCategoria.isEmpty()) {
             return livrosPorCategoria;
-        } else {
-            throw new Exception("Nenhumm livro na biblioteca foi localizado com a categoria " + categoria);
         }
+        
+        throw new Exception("Nenhumm livro na biblioteca foi localizado com a categoria " + categoria);
     }
 }
